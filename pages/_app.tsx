01c5374fdb773a14af6b92547f82f6c212a7746b
Jesus David Barrios <jesusdavid444@gmail.com>
2022-05-08 17:28:58 -0500

    Connection with Moralis

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { RobinhoodProvider } from '../context/RobinhoodContext'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { RobinhoodProvider } from "../context/RobinhoodContext";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RobinhoodProvider>
-      <Component {...pageProps} />
-    </RobinhoodProvider>
-  )
+    <MoralisProvider
+      serverUrl='https://pxwwfao9cuil.usemoralis.com:2053/server'
+      appId='BEBfHzrGVaaklHu4NkD1Mek2asPsptJb6JEMQeFq'
+    >
+      <RobinhoodProvider>
+        <Component {...pageProps} />
+      </RobinhoodProvider>
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
